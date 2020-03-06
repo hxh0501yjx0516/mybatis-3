c537b07c14a46adc83ca277bb90c5efdeaaa1c99
胡宣化 <huxhua163@163.com>
2020-03-06 17:32:22 +0800

    学习mybatis

--- src/main/java/org/apache/ibatis/logging/Log.java
+++ src/main/java/org/apache/ibatis/logging/Log.java
@@ -1,22 +1,25 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.logging;
 
 /**
  * @author Clinton Begin
+ * 这里mybatis自己定义的日志接口
+ * 目的：兼容多种日志框架
+ * 描述：级别越低，日志量越大
  */
 public interface Log {
 
@@ -26,12 +29,25 @@ public interface Log {
 
   void error(String s, Throwable e);
 
+  /*
+  级别高于warn,只虽然发生错误，也不影响系统运行
+   */
   void error(String s);
 
+  /*
+  开发过程中打印 一些运行日志
+   */
   void debug(String s);
 
+  /*
+  较低级别，一般不用
+   */
   void trace(String s);
 
+  /*
+  级别高于 info, debug
+    可能会出现潜在的错误，给程序员一些提示
+   */
   void warn(String s);
 
 }
